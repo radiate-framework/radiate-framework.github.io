3bac071f3d985dcb9c819ad18ab62dd29e0d7245
Ben Rutland <benrutland@hotmail.com>
2021-02-15 17:41:06 +0000

    Initial commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: "Radiate Docs",
+  description: "A WordPress theme and plugin framework",
+  base: "/radiate-docs/",
+  lang: "en-GB",
+  themeConfig: {
+    sidebar: [{ text: "Geting started", link: "/" }],
+  },
+};
