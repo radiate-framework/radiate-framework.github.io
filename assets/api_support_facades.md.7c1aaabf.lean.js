ffa3d4e16e56270772b6108e814892d1d33184a9
BenRutlandWeb <benrutland@hotmail.com>
2021-02-22 23:09:53 +0000

    Deploying to gh-pages from @ radiate-framework/radiate-framework.github.io@fc839c27339840cc2722368a6cf03b9de6493812 🚀

--- assets/api_support_facades.md.7c1aaabf.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as t,c as d,d as e}from"./app.b8d8e604.js";const a='{"title":"Facades","description":"","frontmatter":{},"relativePath":"api/support/facades.md","lastUpdated":1613998048279}',r={},i=e('',4);r.render=function(e,a,r,o,c,s){return t(),d("div",null,[i])};export default r;export{a as __pageData};
